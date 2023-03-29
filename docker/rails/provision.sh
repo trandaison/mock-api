65bbb12f217d2ca305895fc6f09357bb7e568d4a
Tran Dai Son <sontd@nal.vn>
2023-03-29 15:02:04 +0700

    Add docker-compose

--- docker/rails/provision.sh
+++ docker/rails/provision.sh
@@ -8,8 +8,9 @@ bundle exec rake db:migrate
 
 rm -f tmp/pids/server.pid
 
-# yarn install
-# bundle exec rails assets:precompile
-bundle exec rails s -p 5000 -b 0.0.0.0
+yarn install
+bundle exec rails assets:precompile
+bundle exec rake rswag:specs:swaggeriz
+bundle exec rails s -p 3000 -b 0.0.0.0
 
 exec "$@"
